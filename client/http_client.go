fb49c10b11db9c3861ae8e326296fc85459311e7
dan-compton <dan@opsee.co>
2016-08-11 01:01:45 -0700

    ok this

--- client/http_client.go
+++ client/http_client.go
@@ -15,193 +15,146 @@
 package client
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
+    "bytes"
+    "encoding/json"
+    "io/ioutil"
+    "net/http"
 
-	"github.com/dan-compton/go-kairosdb/builder"
-	"github.com/dan-compton/go-kairosdb/response"
+    "github.com/dan-compton/go-kairosdb/response"
 )
 
 var (
-	api_version    = "/api/v1"
-	datapoints_ep  = api_version + "/datapoints"
-	query_ep       = api_version + "/datapoints/query"
-	version_ep     = api_version + "/version"
-	health_ep      = api_version + "/health/check"
-	metricnames_ep = api_version + "/metricnames"
-	tagnames_ep    = api_version + "/tagnames"
-	tagvalues_ep   = api_version + "/tagvalues"
-	delmetric_ep   = api_version + "/metric/"
+    api_version    = "/api/v1"
+    datapoints_ep  = api_version + "/datapoints"
+    query_ep       = api_version + "/datapoints/query"
+    version_ep     = api_version + "/version"
+    health_ep      = api_version + "/health/check"
+    metricnames_ep = api_version + "/metricnames"
+    tagnames_ep    = api_version + "/tagnames"
+    tagvalues_ep   = api_version + "/tagvalues"
+    delmetric_ep   = api_version + "/metric/"
 )
 
 // This is the type that implements the Client interface.
 type httpClient struct {
-	serverAddress string
+    serverAddress string
 }
 
 func NewHttpClient(serverAddress string) Client {
-	return &httpClient{
-		serverAddress: serverAddress,
-	}
+    return &httpClient{
+        serverAddress: serverAddress,
+    }
 }
 
-// Returns a list of all metrics names.
-func (hc *httpClient) GetMetricNames() (*response.GetResponse, error) {
-	return hc.get(hc.serverAddress + metricnames_ep)
-}
+// Queries KairosDB using the query built using the protobuf types
+func (hc *httpClient) Query(qb kairosdb.QueryMetricsRequest) (*response.QueryResponse, error) {
+    // Get the JSON representation of the query.
+    data, err : json.Marshal(qb)
+    if err != nil {
+        return nil, err
+    }
 
-// Returns a list of all tag names.
-func (hc *httpClient) GetTagNames() (*response.GetResponse, error) {
-	return hc.get(hc.serverAddress + tagnames_ep)
-}
-
-// Returns a list of all tag values.
-func (hc *httpClient) GetTagValues() (*response.GetResponse, error) {
-	return hc.get(hc.serverAddress + tagvalues_ep)
-}
-
-// Queries KairosDB using the query built using builder.
-func (hc *httpClient) Query(qb builder.QueryBuilder) (*response.QueryResponse, error) {
-	// Get the JSON representation of the query.
-	data, err := qb.Build()
-	if err != nil {
-		return nil, err
-	}
-
-	return hc.postQuery(hc.serverAddress+query_ep, data)
-}
-
-// Sends metrics from the builder to the KairosDB server.
-func (hc *httpClient) PushMetrics(mb builder.MetricBuilder) (*response.Response, error) {
-	data, err := mb.Build()
-	if err != nil {
-		return nil, err
-	}
-
-	return hc.postData(hc.serverAddress+datapoints_ep, data)
-}
-
-// Deletes a metric. This is the metric and all its datapoints.
-func (hc *httpClient) DeleteMetric(name string) (*response.Response, error) {
-	return hc.delete(hc.serverAddress + delmetric_ep + name)
-}
-
-// TODO: Deletes data in KairosDB using the query built by the builder.
-func (hc *httpClient) Delete(builder builder.QueryBuilder) (*response.Response, error) {
-	return nil, nil
-}
-
-// Checks the health of the KairosDB Server.
-func (hc *httpClient) HealthCheck() (*response.Response, error) {
-	resp, err := hc.sendRequest(hc.serverAddress+health_ep, "GET")
-	if err != nil {
-		return nil, err
-	}
-
-	r := &response.Response{}
-	r.SetStatusCode(resp.StatusCode)
-	return r, nil
+    return hc.postQuery(hc.serverAddress+query_ep, data)
 }
 
 func (hc *httpClient) sendRequest(url, method string) (*http.Response, error) {
-	req, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("accept", "application/json")
-	cli := &http.Client{}
+    req, err := http.NewRequest(method, url, nil)
+    if err != nil {
+        return nil, err
+    }
+    req.Header.Add("accept", "application/json")
+    cli := &http.Client{}
 
-	return cli.Do(req)
+    return cli.Do(req)
 }
 
 func (hc *httpClient) httpRespToResponse(httpResp *http.Response) (*response.Response, error) {
-	resp := &response.Response{}
-	resp.SetStatusCode(httpResp.StatusCode)
+    resp := &response.Response{}
+    resp.SetStatusCode(httpResp.StatusCode)
 
-	if httpResp.StatusCode != http.StatusNoContent {
-		// If the request has failed, then read the response body.
-		defer httpResp.Body.Close()
-		contents, err := ioutil.ReadAll(httpResp.Body)
-		if err != nil {
-			return nil, err
-		} else {
-			// Unmarshal the contents into Response object.
-			err = json.Unmarshal(contents, resp)
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
+    if httpResp.StatusCode != http.StatusNoContent {
+        // If the request has failed, then read the response body.
+        defer httpResp.Body.Close()
+        contents, err := ioutil.ReadAll(httpResp.Body)
+        if err != nil {
+            return nil, err
+        } else {
+            // Unmarshal the contents into Response object.
+            err = json.Unmarshal(contents, resp)
+            if err != nil {
+                return nil, err
+            }
+        }
+    }
 
-	return resp, nil
+    return resp, nil
 }
 
 func (hc *httpClient) httpRespToQueryResponse(httpResp *http.Response) (*response.QueryResponse, error) {
-	// Read the HTTP response body.
-	defer httpResp.Body.Close()
-	contents, err := ioutil.ReadAll(httpResp.Body)
-	if err != nil {
-		return nil, err
-	}
+    // Read the HTTP response body.
+    defer httpResp.Body.Close()
+    contents, err := ioutil.ReadAll(httpResp.Body)
+    if err != nil {
+        return nil, err
+    }
 
-	qr := response.NewQueryResponse(httpResp.StatusCode)
 
-	// Unmarshal the contents into QueryResponse object.
-	err = json.Unmarshal(contents, qr)
-	if err != nil {
-		return nil, err
-	}
+    qr := &QueryMetricsResponse{}
 
-	return qr, nil
+    // Unmarshal the contents into QueryResponse object.
+    err = json.Unmarshal(contents, qr)
+    if err != nil {
+        return nil, err
+    }
+
+    return qr, nil
 }
 
 func (hc *httpClient) get(url string) (*response.GetResponse, error) {
-	resp, err := hc.sendRequest(url, "GET")
-	if err != nil {
-		return nil, err
-	}
+    resp, err := hc.sendRequest(url, "GET")
+    if err != nil {
+        return nil, err
+    }
 
-	defer resp.Body.Close()
-	contents, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	} else {
-		gr := response.NewGetResponse(resp.StatusCode)
+    defer resp.Body.Close()
+    contents, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return nil, err
+    } else {
+        gr := response.NewGetResponse(resp.StatusCode)
 
-		err = json.Unmarshal(contents, gr)
-		if err != nil {
-			return nil, err
-		}
+        err = json.Unmarshal(contents, gr)
+        if err != nil {
+            return nil, err
+        }
 
-		return gr, nil
-	}
+        return gr, nil
+    }
 }
 
 func (hc *httpClient) postData(url string, data []byte) (*response.Response, error) {
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
-	if err != nil {
-		return nil, err
-	}
+    resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+    if err != nil {
+        return nil, err
+    }
 
-	return hc.httpRespToResponse(resp)
+    return hc.httpRespToResponse(resp)
 }
 
 func (hc *httpClient) postQuery(url string, data []byte) (*response.QueryResponse, error) {
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
-	if err != nil {
-		return nil, err
-	}
+    resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+    if err != nil {
+        return nil, err
+    }
 
-	return hc.httpRespToQueryResponse(resp)
+    return hc.httpRespToQueryResponse(resp)
 }
 
 func (hc *httpClient) delete(url string) (*response.Response, error) {
-	resp, err := hc.sendRequest(url, "DELETE")
-	if err != nil {
-		return nil, err
-	}
+    resp, err := hc.sendRequest(url, "DELETE")
+    if err != nil {
+        return nil, err
+    }
 
-	return hc.httpRespToResponse(resp)
+    return hc.httpRespToResponse(resp)
 }
